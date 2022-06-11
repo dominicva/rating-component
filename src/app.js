7bba71977dd9283af198359099c189af131feb2d
Dom van Almsick <dominicvana@gmail.com>
2022-06-11 11:20:53 -0500

    feat: track selected rating

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const radioBtns = document.querySelectorAll('label');
 
+let selectedRating;
+
 for (const btn of radioBtns) {
   btn.addEventListener('click', () => {
     // clear out any other orange state
     radioBtns.forEach(b => b.classList.remove('orange'));
     // make this one orange
     btn.classList.toggle('orange');
+    // keep track of selected rating
+    selectedRating = Number(btn.innerText);
   });
 }
